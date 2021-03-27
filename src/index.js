4cf5f91a4b6da97dd0a67b5fab8bf258e792914d
ileudo <Rios111@yandex.ru>
2021-03-27 18:32:46 +0300

    feat:implement hanoi-tower task

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-function getSeason(date) {
-	let season;
-	if (!date) return 'Unable to determine the time of year!';
-	if ((Object.prototype.toString.apply(date)) !== '[object Date]') { throw new Error('Error') };
-	if (date.getMonth() <= 1 || date.getMonth() === 11) { season = 'winter' }
-	else if (date.getMonth() <= 4) { season = 'spring' }
-	else if (date.getMonth() <= 7) { season = 'summer' }
-	else if (date.getMonth() <= 10) { season = 'autumn' };
-	return season;
-};
+function calculateHanoi(disksNumber, turnsSpeed) {
+	const obj = {
+		turns: 0,
+		seconds: 0,
+	}
+	obj.turns = (2 ** disksNumber) - 1;
+	const turnsPerSec = turnsSpeed / 3600;
+	obj.seconds = Math.floor(obj.turns / turnsPerSec);
+	return obj;
+}
 
+console.log(calculateHanoi(9, 4308));
--- src/what-season.js
+++ src/what-season.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 module.exports = function getSeason(date) {
 	let season;
 	if (!date) return 'Unable to determine the time of year!';
-	if ((Object.prototype.toString.apply(date)) !== '[object Date]') { throw new Error('Error') };
+	if ((Object.prototype.toString.call(date)) !== '[object Date]') { return false };
 	if (date.getMonth() <= 1 || date.getMonth() === 11) { season = 'winter' }
 	else if (date.getMonth() <= 4) { season = 'spring' }
 	else if (date.getMonth() <= 7) { season = 'summer' }
