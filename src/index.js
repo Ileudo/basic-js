776dfa5af595a28d98eadac789027668e09b4951
ileudo <Rios111@yandex.ru>
2021-03-27 20:50:02 +0300

    init: test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,39 @@
-function calculateHanoi(disksNumber, turnsSpeed) {
-	const obj = {
-		turns: 0,
-		seconds: 0,
+function transform(arr) {
+	let arrNew = new Array();
+	arrNew = arr;
+	for (i = 0; i < arrNew.length; i++) {
+		if (arrNew[i] === '--discard-next') {
+			if (arrNew[i + 2] === '--double-prev') { arrNew.splice(i, 3) } else { arrNew.splice(i, 2) }
+		}
+		else if (arrNew[i] === '--discard-prev') {
+			if (i === 0) { arrNew.splice(i, 1) } else { arrNew.splice(i, 2) }
+		}
+		else if (arrNew[i] === '--double-next') {
+			if (i === arrNew.length - 1) { arrNew.splice(i, 1) } else { arrNew[i] = arrNew[i + 1] }
+		}
+		else if (arrNew[i] === '--double-prev') {
+			if (i === 0) { arrNew.splice(i, 1) } else { arrNew[i + 1] = arrNew[i] }
+		}
 	}
-	obj.turns = (2 ** disksNumber) - 1;
-	const turnsPerSec = turnsSpeed / 3600;
-	obj.seconds = Math.floor(obj.turns / turnsPerSec);
-	return obj;
+	return arrNew;
 }
 
-console.log(calculateHanoi(9, 4308));
+console.log(transform([1, 2, 3, '--double-next', 4, 5]));
+console.log(transform(["--discard-prev", 1, 2, 3]));
+console.log(transform(['--discard-prev', 1, 2, 3]));
+console.log(transform(['--double-prev', 1, 2, 3]));
+console.log(transform([1, 2, 3, '--double-next']));
+console.log(transform([1, 2, 3, '--discard-next']));
+console.log(transform([1, 2, 3, '--discard-next', 1337, '--double-prev', 4, 5]));
+
+// function transform(arr) {
+// 	let arrNew = new Array();
+// 	for (i = 0; i < arr.length; i++) {
+// 		if (arr[i] === '--discard-next') { if (i === i + 1) { continue } }
+// 		else if (arr[i] === '--discard-prev') { arrNew.pop() }
+// 		else if (arr[i] === '--double-next') { arrNew.push(arr[i + 1]) }
+// 		else if (arr[i] === '--double-prev') { arrNew.push(arr[i - 1]) }
+// 		else arrNew.push(arr[i])
+// 	};
+// 	return arrNew;
+// }
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,21 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function transform(arr) {
+	let arrNew = new Array();
+	arrNew = arr;
+	for (i = 0; i < arrNew.length; i++) {
+		if (arrNew[i] === '--discard-next') {
+			if (arrNew[i + 2] === '--double-prev') { arrNew.splice(i, 3) } else { arrNew.splice(i, 2) }
+		}
+		else if (arrNew[i] === '--discard-prev') {
+			if (i === 0) { arrNew.splice(i, 1) } else { arrNew.splice(i, 2) }
+		}
+		else if (arrNew[i] === '--double-next') {
+			if (i === arrNew.length - 1) { arrNew.splice(i, 1) } else { arrNew[i] = arrNew[i + 1] }
+		}
+		else if (arrNew[i] === '--double-prev') {
+			if (i === 0) { arrNew.splice(i, 1) } else { arrNew[i + 1] = arrNew[i] }
+		}
+	}
+	return arrNew;
+}
